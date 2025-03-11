307ea53d4c9f917f71467f6efff99390cf198048
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-11 09:59:47 +0100

    sequential solution

--- src/main/scala/Frequency.scala
+++ src/main/scala/Frequency.scala
@@ -1,2 +1,12 @@
 object Frequency:
-   def frequency(numWorkers: Int, texts: Seq[String]): Map[Char, Int] = ???
+   def frequency(numWorkers: Int, texts: Seq[String]): Map[Char, Int] =
+      val t =
+         if texts.isEmpty then ""
+         else texts.reduce((s, s2) => s.concat(s2))
+
+      "[a-zöü]*".r
+         .findAllIn(t.toLowerCase)
+         .mkString
+         .to(scala.collection.Seq)
+         .groupBy(identity)
+         .map((k, v) => k -> v.size)
