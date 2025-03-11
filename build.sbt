39400204bf68b3656f4e3a4d65ca69a14335ddf4
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-11 12:33:07 +0100

    parallel solution

--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
 scalaVersion := "3.6.4"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.19" % Test
+libraryDependencies +=
+   "org.scala-lang.modules" %% "scala-parallel-collections" % "1.2.0"
--- src/main/scala/Frequency.scala
+++ src/main/scala/Frequency.scala
@@ -1,11 +1,9 @@
+import scala.collection.parallel.CollectionConverters.*
+
 object Frequency:
    def frequency(numWorkers: Int, texts: Seq[String]): Map[Char, Int] =
-      val t =
-         if texts.isEmpty then ""
-         else texts.reduce((s, s2) => s.concat(s2))
-      "[a-zöü]*".r
-         .findAllIn(t.toLowerCase)
-         .mkString
-         .toSeq
-         .groupBy(identity)
-         .map((k, v) => k -> v.size)
+      texts.par
+         .map(_.toLowerCase.filter(_.isLetter).groupMapReduce(identity)(_ => 1)(_ + _))
+         .reduceOption((m1, m2) => m1 ++ m2.map(t => (t._1, t._2 + m1.getOrElse(t._1, 0))))
+         .getOrElse(Map.empty)
+
