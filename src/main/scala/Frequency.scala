fe670436b7664bd2e5123fd607c32dd44d73d2d3
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-11 13:07:55 +0100

    Concurrent Future solution

--- src/main/scala/Frequency.scala
+++ src/main/scala/Frequency.scala
@@ -1,5 +1,4 @@
 import java.util.concurrent.Executors
-import scala.collection.parallel.CollectionConverters.*
 import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutorService, Future}
 import scala.concurrent.duration.Duration
 
@@ -9,7 +8,7 @@ object Frequency:
          ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(numWorkers))
 
       val f = Future(
-        texts.par
+        texts
            .map(_.toLowerCase.filter(_.isLetter).groupMapReduce(identity)(_ => 1)(_ + _))
            .reduceOption((m1, m2) => m1 ++ m2.map(t => (t._1, t._2 + m1.getOrElse(t._1, 0))))
            .getOrElse(Map.empty))
